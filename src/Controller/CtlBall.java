4df7f7d636f4bd67b6461e06eff9522e5e4815e1
Daryl110 <daryl.8220@gmail.com>
2019-08-17 19:56:54 +0100

    comentarios en metodos para mover bola

--- src/Controller/CtlBall.java
+++ src/Controller/CtlBall.java
@@ -27,6 +27,7 @@ public class CtlBall extends JComponent implements Runnable {
     private final JComponent panel;
     public static int time;
     private final CtlBar barController;
+    private final int moveVelocity;
 
     private enum Movements {
         top, down, topLeft, topRight, downLeft, downRight
@@ -41,6 +42,7 @@ public class CtlBall extends JComponent implements Runnable {
         this.image = new ImageIcon(getClass().getResource("..\\Resourses\\ballOfSoccer.jpg")).getImage();
         this.barController = barController;
         this.ballState = Movements.down;
+        this.moveVelocity = 5;
     }
 
     @Override
@@ -71,72 +73,70 @@ public class CtlBall extends JComponent implements Runnable {
         this.ball = ball;
     }
 
-    int velocity = 5;
-
     public void move() {
-        switch (ballState) {
-            case down:
-                if (this.ball.getPosY() < panel.getHeight() - 30) {
-                    this.ball.setPosY(this.ball.getPosY() + this.velocity);
-                    this.validateHitBar();
-                } else {
-                    this.deleteBall();
+        switch (ballState) {//estado del movimiento de la bola
+            case down:// Movimiento hacia abajo
+                if (this.ball.getPosY() < panel.getHeight() - 30) {//si la bola no ha tocado la parte de abajo
+                    this.ball.setPosY(this.ball.getPosY() + this.moveVelocity);//la bola seguira bajando hasta que llegue a la parte de abajo
+                    this.validateHitBar();//valida el toque con la barra
+                } else {//si la bola no toco la barra y bajo todo
+                    this.deleteBall();//elimina la bola tanto del arreglo como del panel
                 }
                 break;
-            case top:
-                if (this.ball.getPosY() >= 0) {
-                    this.ball.setPosY(this.ball.getPosY() - this.velocity);
-                } else {
-                    this.ballState = Movements.down;
+            case top://Movimiento hacia arriba
+                if (this.ball.getPosY() >= 0) {//si la bola no ha llegado a la parte de arriba
+                    this.ball.setPosY(this.ball.getPosY() - this.moveVelocity);//la bola seguira subiendo hasta que toque la parte de arriba
+                } else {//si la bola toca la parte de arriba
+                    this.ballState = Movements.down;//se camia estado a movimiento para que rebote y se mueva hacia abajo
                 }
                 break;
-            case topLeft:
-                if (this.ball.getPosX() >= 0 && this.ball.getPosY() >= 0) {
-                    this.ball.setPosX(this.ball.getPosX() - this.velocity);
-                    this.ball.setPosY(this.ball.getPosY() - this.velocity);
-                    if (this.ball.getPosY() <= 0 && this.ball.getPosX() >= 0) {
-                        this.ballState = Movements.downLeft;
+            case topLeft://Movimiento hacia arribaIzquierda
+                if (this.ball.getPosX() >= 0 && this.ball.getPosY() >= 0) {//si la bola no ha llegado a la parte izquierda
+                    this.ball.setPosX(this.ball.getPosX() - this.moveVelocity);//mover hacia el lado izquierdo
+                    this.ball.setPosY(this.ball.getPosY() - this.moveVelocity);//mover hacia arriba
+                    if (this.ball.getPosY() <= 0 && this.ball.getPosX() >= 0) {//si la bola toca la parte de arriba
+                        this.ballState = Movements.downLeft;//se cambia el estado a movimiento para que se mueva hacia abajo izquierda
                     }
-                    if (this.ball.getPosX() <= 0 && this.ball.getPosY() >= 0) {
-                        this.ballState = Movements.topRight;
-                    }
-                }
-                break;
-            case downLeft:
-                if (this.ball.getPosX() >= 0 && this.ball.getPosY() <= this.panel.getHeight() - 30) {
-                    this.ball.setPosX(this.ball.getPosX() - this.velocity);
-                    this.ball.setPosY(this.ball.getPosY() + this.velocity);
-                    if (this.ball.getPosX() <= 0 && this.ball.getPosY() >= 0) {
-                        this.ballState = Movements.downRight;
-                    }
-                    this.validateHitBar();
-                    if (this.ball.getPosY() > panel.getHeight() - 30) {
-                        this.deleteBall();
+                    if (this.ball.getPosX() <= 0 && this.ball.getPosY() >= 0) {//si la bola toca la parte izquierda
+                        this.ballState = Movements.topRight;//se cambia el estado a movimiento para que se mueva hacia arriba derecha
                     }
                 }
                 break;
-            case downRight:
-                if (this.ball.getPosY() <= this.panel.getHeight() - 30) {
-                    this.ball.setPosX(this.ball.getPosX() + this.velocity);
-                    this.ball.setPosY(this.ball.getPosY() + this.velocity);
-                    if (this.ball.getPosX() >= this.panel.getWidth() - 30 && this.ball.getPosY() >= 0) {
-                        this.ballState = Movements.downLeft;
+            case downLeft://Movimiento hacia abajoIzquierda
+                if (this.ball.getPosX() >= 0 && this.ball.getPosY() <= this.panel.getHeight() - 30) {//si la bola no ha tocado la parte de abajo y si no ha tocado la parte izquierda
+                    this.ball.setPosX(this.ball.getPosX() - this.moveVelocity);//mover hacia la izquierda
+                    this.ball.setPosY(this.ball.getPosY() + this.moveVelocity);//mover hacia abajo
+                    if (this.ball.getPosX() <= 0 && this.ball.getPosY() >= 0) {//si la bola toca la parte izquierda
+                        this.ballState = Movements.downRight;//Se cambia el estado a movimiento para que se mueva abajo derecha
                     }
-                    this.validateHitBar();
-                    if (this.ball.getPosY() > panel.getHeight() - 30) {
-                        this.deleteBall();
+                    this.validateHitBar();//valida el toque de la bola con la barra
+                    if (this.ball.getPosY() > panel.getHeight() - 30) {//si la bola toca la parte de abajo
+                        this.deleteBall();//se elimina la bola tanto del arreglo como del panel
                     }
                 }
                 break;
-            case topRight:
-                if (this.ball.getPosX() <= this.panel.getWidth() - 30 && this.ball.getPosY() >= 0) {
-                    this.ball.setPosX(this.ball.getPosX() + this.velocity);
-                    this.ball.setPosY(this.ball.getPosY() - this.velocity);
-                    if (this.ball.getPosY() <= 0 && this.ball.getPosX() >= 0) {
-                        this.ballState = Movements.downRight;
+            case downRight://Movimiento hacia abajoDerecha
+                if (this.ball.getPosY() <= this.panel.getHeight() - 30 && this.ball.getPosX() <= this.panel.getWidth() - 30) {//si la bola no ha tocado la parte de abajo ni la parte derecha
+                    this.ball.setPosX(this.ball.getPosX() + this.moveVelocity);//mover hacia la derecha
+                    this.ball.setPosY(this.ball.getPosY() + this.moveVelocity);//mover hacia abajo
+                    if (this.ball.getPosX() >= this.panel.getWidth() - 30 && this.ball.getPosY() >= 0) {//si la bola toca la parte derecha
+                        this.ballState = Movements.downLeft;//Se cambia el estado a movimiento para que se mueva abajo izquierda
                     }
-                    if (this.ball.getPosX() >= this.panel.getWidth() - 30 && this.ball.getPosY() >= 0) {
-                        this.ballState = Movements.topLeft;
+                    this.validateHitBar();//Valida el toque de la bola con la barra
+                    if (this.ball.getPosY() > panel.getHeight() - 30) {//si la bola toca la parte de abajo
+                        this.deleteBall();//se elimina la bola tanto del arreglo como del panel
+                    }
+                }
+                break;
+            case topRight://Movimiento arribaDerecha
+                if (this.ball.getPosX() <= this.panel.getWidth() - 30 && this.ball.getPosY() >= 0) {//si la bola no ha tocado la parte derecha y si la bola no ha tocado la parte de arriba
+                    this.ball.setPosX(this.ball.getPosX() + this.moveVelocity);//mover a la derecha
+                    this.ball.setPosY(this.ball.getPosY() - this.moveVelocity);//mover hacia arriba
+                    if (this.ball.getPosY() <= 0 && this.ball.getPosX() >= 0) {//si la bola toca la parte de arriba
+                        this.ballState = Movements.downRight;//Se cambia el estado a movimiento para que se mueva abajo derecha
+                    }
+                    if (this.ball.getPosX() >= this.panel.getWidth() - 30 && this.ball.getPosY() >= 0) {//si la bola toca la parte derecha
+                        this.ballState = Movements.topLeft;//Se cambia el estado a movimiento para que se mueva arriba izquierda
                     }
                 }
                 break;
@@ -147,32 +147,32 @@ public class CtlBall extends JComponent implements Runnable {
     private void validateHitBar() {
         if ((this.ball.getPosX() >= this.barController.getBar().getPosX()
                 && this.ball.getPosX() <= (this.barController.getBar().getPosX() + (this.barController.getBar().getWidth() / 3)))
-                && (this.ball.getPosY() > this.barController.getBar().getPosY() - 30)) {
-            this.ballState = Movements.topLeft;
+                && (this.ball.getPosY() > this.barController.getBar().getPosY() - 30)) {//si la bola esta en el rango de 1/3 barra (izquierda)
+            this.ballState = Movements.topLeft;//se cambia el estado a movimiento para que se mueva arriba izquierda
         } else if ((this.ball.getPosX() >= (this.barController.getBar().getPosX() + (this.barController.getBar().getWidth() / 3))
                 && this.ball.getPosX() <= (this.barController.getBar().getPosX() + ((this.barController.getBar().getWidth() / 3) * 2)))
-                && (this.ball.getPosY() > this.barController.getBar().getPosY() - 30)) {
-            this.ballState = Movements.top;
+                && (this.ball.getPosY() > this.barController.getBar().getPosY() - 30)) {//si la bola esta en el rango de 2/3 barra (centro)
+            this.ballState = Movements.top;//se cambia el estado a movimiento para que se mueva arriba
         } else if ((this.ball.getPosX() >= (this.barController.getBar().getPosX() + ((this.barController.getBar().getWidth() / 3) * 2))
                 && this.ball.getPosX() <= (this.barController.getBar().getPosX() + this.barController.getBar().getWidth()))
-                && (this.ball.getPosY() > this.barController.getBar().getPosY() - 30)) {
-            this.ballState = Movements.topRight;
+                && (this.ball.getPosY() > this.barController.getBar().getPosY() - 30)) {//si la bola esta en el rango de 3/3 barra (derecha)
+            this.ballState = Movements.topRight;//se cambia el estado a movimiento para que se mueva arriba derecha
         }
     }
 
     private void deleteBall() {
-        this.panel.remove(this);
-        time -= 1;
-        FrmMain.threadsBallsControllers.remove(this);
-        FrmMain.lblNumberBall.setText("" + FrmMain.threadsBallsControllers.size());
+        this.panel.remove(this);//elimina la bola del panel para no dibujarla mas
+        time -= 1;//reduce el tiempo 1 por la bola que se fue (-1 milisegundo de silencio por esa bola)
+        FrmMain.threadsBallsControllers.remove(this);//remueve la bola del arreglo
+        FrmMain.lblNumberBall.setText("" + FrmMain.threadsBallsControllers.size());//pinta el numero actual de bolas en el arreglo
     }
 
     private int calculatePosition(String orientation) {
         int number = 0;
         if (orientation.equalsIgnoreCase("x")) {
-            number = ThreadLocalRandom.current().nextInt(0, this.panel.getWidth() - 30);
+            number = ThreadLocalRandom.current().nextInt(0, this.panel.getWidth() - 30);//numeros aleatorios para el ancho
         }else{
-            number = ThreadLocalRandom.current().nextInt(0, this.panel.getHeight() - 30);
+            number = ThreadLocalRandom.current().nextInt(0, this.panel.getHeight() - 30);//numeros aleatotios para el alto
         }
         return number;
     }
